8b2951c2ba5c14de04426dfeded762dfe0642c7a
Nzingani <isabelnzingani@outlook.com>
2018-07-27 11:22:11 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>P3ex6</title>
+  </head>
+  <body>
+    <?php
+    $not= 20;
+while($not > 5)
+{
+echo 'c\'est presque bon';
+$not --;
+}
+  ?>
+</body>
+</html>
